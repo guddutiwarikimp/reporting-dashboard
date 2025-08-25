499cadb9e3039174e2ac7b793b0c644ed3358943
aliasgerratlamwala-eng <aliasger.ratlamwala@kimp.xyz>
2025-08-25 14:46:51 +0530

    border change

--- src/components/reporting/TextStats.tsx
+++ src/components/reporting/TextStats.tsx
@@ -3,14 +3,12 @@ import { formatNumber } from '@/utils'
 
 const TextStats = ({ stats }: { stats: any }) => {
     return (
-        <Card className="[&_.ant-card-body]:!p-3 relative shadow">
+        <Card className="[&_.ant-card-body]:!p-3 relative shadow border !border-gray-300/60">
             <div className="flex flex-col gap-2">
-                <div>
-                    <div className="text-sm text-gray-500 font-semibold dark:text-gray-300">{stats.title}</div>
-                    <div className={`text-2xl font-bold text-gray-800`}>{formatNumber(stats.value)}{stats.title === "Delivery Rate" ? "%" : ""}</div>
+                <div className="text-sm text-gray-500 font-semibold dark:text-gray-300">{stats.title}</div>
+                <div className={`text-2xl font-bold text-gray-800`}>{formatNumber(stats.value)}{stats.title === "Delivery Rate" ? "%" : ""}</div>
 
-                    {stats.people && <div className="text-xs text-gray-500 absolute top-[14px] right-[18px]">{formatNumber(stats.people)} people</div>}
-                </div>
+                {stats.people && <div className="text-xs text-gray-500 absolute top-[14px] right-[18px]">{formatNumber(stats.people)} people</div>}
             </div>
         </Card>
     )
